18f4f0f8660d2fecf790f389efa816a7aae27af8
sfinch6 <sfinch6@gatech.edu>
2017-04-10 21:31:42 -0400

    add user

--- python/addUser.py
+++ python/addUser.py
@@ -4,18 +4,13 @@ from Tkinter import *
 class AddUser:
     def __init__(self, master):
 
-        db = MySQLdb.connect(
-            host="localhost",
-            user="root",
-            passwd="yourMySQLPassword",
-            db="HERO")
 
-        curr = db.cursor()
-
+    
         self.frame = Frame(master)
         self.frame.pack()
 
         self.name = Label(self.frame, text="Name").grid(row=1)
+        global entry1
         self.entry1 = Entry(self.frame).grid(row=1, column=1)
 
         self.username = Label(self.frame, text="Username").grid(row=2)
@@ -29,13 +24,36 @@ class AddUser:
         self.variable.set("Regular")
         self.menu = OptionMenu(self.frame, self.variable, "Regular", "Manager", "Admin").grid(row=4, column=1)
 
-        self.add = Button(self.frame, text="Add User").grid(row=6, column=3)
+        self.add = Button(self.frame, text="Add User", command = self.addUser).grid(row=6, column=3)
         self.back = Button(self.frame, text="Go Back").grid(row=6, column=0)
+
+
+    def addUser(self):
+
+        db = MySQLdb.connect(
+            host="localhost",
+            user="root",
+            passwd="Darling",
+            db="HERO")
+
+        curr = db.cursor()
+        
+        name = entry1.get()
+        user = entry2.get()
+        password = entry3.get()
+        level = variable.get()
+
+        #sql insert new user execution
+        
 
         curr.close()
         db.close()
 
+
 master = Tk()
 addUserPage = AddUser(master)
 master.mainloop()
+
+
+
 
