7e33caf7ec9d365369f5816b6aaa7b76a155734c
Chung Fuk Yuen, Jean Daniel <danielchung@lawn-128-61-21-16.lawn.gatech.edu>
2017-04-10 23:31:35 -0400

    changed login to reflect extra name field in the user table

--- python/delUser.py
+++ python/delUser.py
@@ -10,11 +10,14 @@ class DeleteUserPage:
 		db = MySQLdb.connect(host = "localhost", user = "root", db = "HERO")
 		curr = db.cursor()
 
-		nameHead = Label(master, text = "Username", font= "Verdana 10 underline")
+		nameHead = Label(master, text = "Nameame", font= "Verdana 10 underline")
 		nameHead.grid(row = 1, column = 0)
 
+		usernameHead = Label(master, text = "Username", font= "Verdana 10 underline")
+		usernameHead.grid(row = 1, column = 1)
+
 		critHead = Label(master, text = "User Type", font= "Verdana 10 underline")
-		critHead.grid(row = 1, column = 1)
+		critHead.grid(row = 1, column = 2)
 
 		self.back = Button(master, text = "Back", command = self.closeWindow)
 		self.back.grid(row = 0, column = 0)
@@ -22,21 +25,53 @@ class DeleteUserPage:
 		curr.execute("SELECT * FROM User")
 		results = curr.fetchall()
 
+
+		# for i in range(len(results)):
+		# 	nameLabel = Label(master, text = results[i][0], font= "Verdana 10")
+		# 	nameLabel.grid(row = 2 + i, column = 0)
+
+		# 	usernameLabel = Label(master, text = results[i][1], font= "Verdana 10")
+		# 	usernameLabel.grid(row = 2 + i, column = 1)
+
+		# 	usertypeLabel = Label(master, text = results[i][3], font= "Verdana 10")
+		# 	usertypeLabel.grid(row = 2 + i, column = 2)
+
+		# 	deleteButton = Button(master, text = "Delete", command = lambda: self.deleteUser(results[i][1]))
+		# 	deleteButton.grid(row = 2 + i, column = 3)
+
+		# curr.close()
+		# db.close()
+
 		count = 0
-		for username,_,usertype in results:
+		for name,username,_,usertype in results:
+			nameLabel = Label(master, text = name, font= "Verdana 10")
+			nameLabel.grid(row = 2 + count, column = 0)
+
 			usernameLabel = Label(master, text = username, font= "Verdana 10")
-			usernameLabel.grid(row = 2 + count, column = 0)
+			usernameLabel.grid(row = 2 + count, column = 1)
 
 			usertypeLabel = Label(master, text = usertype, font= "Verdana 10")
-			usertypeLabel.grid(row = 2 + count, column = 1)
+			usertypeLabel.grid(row = 2 + count, column = 2)
 
-			deleteButton = Button(master, text = "Delete", command = self.closeWindow)
-			deleteButton.grid(row = 2 + count, column = 2)
+			deleteButton = Button(master, text = "Delete", command = lambda: self.closeWindow()) # TODO: Fix deleteUser
+			deleteButton.grid(row = 2 + count, column = 3)
 			count += 1
 
+		curr.close()
+		db.close()
 
-	# def deleteUser(self, curr, username):
-	# 	curr.execute()
+
+	def deleteUser(self, username):
+
+		print username
+		# db = MySQLdb.connect(host = "localhost", user = "root", db = "HERO")
+		# curr = db.cursor()
+
+		# curr.execute("DELETE FROM User WHERE Username = %s", (username,))
+		# db.commit()
+
+		# curr.close()
+		# db.close()
 
 	def closeWindow(self):
 		self.master.destroy()
