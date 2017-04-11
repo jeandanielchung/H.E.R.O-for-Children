1b6ee3be4c731f31502fc8e3b1f01db4f1355e6a
Mateo Rodriguez <matrod1997@gmail.com>
2017-04-11 16:24:22 -0400

    done did another thing! connected nameBirthEntry.py to db

--- python/nameBirthEntry.py
+++ python/nameBirthEntry.py
@@ -1,47 +1,121 @@
 from Tkinter import *
+import MySQLdb
 
 
 class nameBirthEntryPage:
-	def __init__(self, master):
-		self.master = master
-		master.title("Name and Birthday Search Results Page")
-		#5 columns at least 3 rows
-		count = Label(master, text = "Total Matches: ")
-		count.grid(row = 0, column = 3)
+        def __init__(self, master):
+                self.master = master
+                master.title("Name and Birthday Search Results Page")
+                #5 columns at least 3 rows
 
-		nameHead = Label(master, text = "Name", font= "Verdana 10 underline")
-		nameHead.grid(row = 1, column = 0)
+                nameHead = Label(master, text = "Name", font= "Verdana 10 underline")
+                nameHead.grid(row = 1, column = 0)
 
-		progHead = Label(master, text = "Program", font= "Verdana 10 underline")
-		progHead.grid(row = 1, column = 1)
+                progHead = Label(master, text = "Program", font= "Verdana 10 underline")
+                progHead.grid(row = 1, column = 1)
 
-		yearHead = Label(master, text = "Year", font= "Verdana 10 underline")
-		yearHead.grid(row = 1, column = 2)
+                yearHead = Label(master, text = "Year", font= "Verdana 10 underline")
+                yearHead.grid(row = 1, column = 2)
 
-		self.back = Button(master, text = "Back", command = self.closeWindow)
-		self.back.grid(row = 0, column = 0)
+                self.back = Button(master, text = "Back", command = self.closeWindow)
+                self.back.grid(row = 0, column = 0)
 
-        #example data
-		for num in range(0,5):
-			name = Label(master, text = "Test Name")
-			name.grid(row = 2 + num, column = 0)
+                ###
+                ID = 2 ##get this ID from another page
+                ###
+                program = "Child Application" #get me too!
+                ###
+                
+                db = MySQLdb.connect( host = "localhost",
+                                      user="root",
+                                      passwd="Mr10371!",
+                                      db="HERO" )
+                curr = db.cursor()
 
-			prog = Label(master, text = "Summer")
-			prog.grid(row = 2 + num, column = 1)
-
-			year = Label(master, text = "2012")
-			year.grid(row = 2 + num, column = 2)
-
-			profBut = Button(master, text = "Profile", command = self.closeWindow)
-			profBut.grid(row = 2 + num, column = 3)
+                #selects all instances of specified ID in child and camper applications
+                curr.execute("SELECT Name_First,Name_Last,Year(Date_Submitted) FROM Childs_Information WHERE ID = %s;",(ID))
+                childNameDate = curr.fetchall()
+                curr.execute("SELECT First_Name,Last_Name,Year(Date_Submitted) FROM Demographic_Information WHERE ID = %s;",(ID))
+                camperNameDate = curr.fetchall()
 
 
+ 
+
+                if program == 'none':
+                        for num in range(len(childNameDate)):
+                                name = Label(master, text = childNameDate[num][0]+' '+childNameDate[num][1])
+                                name.grid(row = 2 + num, column = 0)
+
+                                prog = Label(master, text = "Child Application")
+                                prog.grid(row = 2 + num, column = 1)
+
+                                year = Label(master, text = childNameDate[num][2])
+                                year.grid(row = 2 + num, column = 2)
+
+                                profBut = Button(master, text = "Profile", command = self.closeWindow)
+                                profBut.grid(row = 2 + num, column = 3)
+
+                        for num in range(len(camperNameDate)):
+                                name = Label(master, text = camperNameDate[num][0]+' '+camperNameDate[num][1])
+                                name.grid(row = 2 + num +len(childNameDate), column = 0)
+
+                                prog = Label(master, text = "Camper Application")
+                                prog.grid(row = 2 + num +len(childNameDate), column = 1)
+
+                                year = Label(master, text = camperNameDate[num][2])
+                                year.grid(row = 2 + num +len(childNameDate), column = 2)
+
+                                profBut = Button(master, text = "Profile", command = self.closeWindow)
+                                profBut.grid(row = 2 + num +len(childNameDate), column = 3)
+
+                        total = len(childNameDate)+len(camperNameDate)
+                        
+                elif program == 'Child Application':
+                        for num in range(len(childNameDate)):
+                                name = Label(master, text = childNameDate[num][0]+' '+childNameDate[num][1])
+                                name.grid(row = 2 + num, column = 0)
+
+                                prog = Label(master, text = "Child Application")
+                                prog.grid(row = 2 + num, column = 1)
+
+                                year = Label(master, text = childNameDate[num][2])
+                                year.grid(row = 2 + num, column = 2)
+
+                                profBut = Button(master, text = "Profile", command = self.closeWindow)
+                        total = len(childNameDate)
+                        
+                else:
+                        for num in range(len(camperNameDate)):
+                                name = Label(master, text = camperNameDate[num][0]+' '+camperNameDate[num][1])
+                                name.grid(row = 2 + num, column = 0)
+
+                                prog = Label(master, text = "Camper Application")
+                                prog.grid(row = 2 + num, column = 1)
+
+                                year = Label(master, text = camperNameDate[num][2])
+                                year.grid(row = 2 + num, column = 2)
+
+                                profBut = Button(master, text = "Profile", command = self.closeWindow)
+                                profBut.grid(row = 2 + num, column = 3)
+                        total = len(camperNameDate)
+
+
+                #print total number of matches
+                count = Label(master, text = "Total Matches: " + str(total))
+                count.grid(row = 0, column = 3)
+                        
+                        
+                        
+                        
 
 
 
-		#possibly add a back button
-	def closeWindow(self):
-		self.master.destroy()
+
+
+
+                #possibly add a back button
+        def closeWindow(self):
+                self.master.destroy()
 
 master = Tk()
 my_gui = nameBirthEntryPage(master)
